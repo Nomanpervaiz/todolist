16e73143605e060416e93f6ad61e5655ba7281c2
Nomanpervaiz <nomanpervaiz035@gmail.com>
2024-06-01 04:06:39 +0500

    add

---
+++ app.js
@@ -0,0 +1,62 @@
+let input = document.getElementById("input");
+let list = document.getElementById("list");
+let iconErase = document.getElementsByClassName(".fa-solid fa-eraser")
+let iconEdit = document.getElementsByClassName(".fa-solid fa-pen-to-square")
+let checkBoox = document.getElementById('checkBoox')
+
+
+function lastMessage() {
+  var scroll = list;
+  scroll.scrollTop = scroll.scrollHeight;
+}
+// add items on enter press function
+function addItemEnter(event) {
+  if (event.keyCode === 13) {
+    
+    list.innerHTML += `<li class = 'inputList' ><input onclick = 'delTag()'  type="checkbox" id ="checkBoox"><input class = 'todo-inp'  type="text" value='${input.value}' disabled><button class='dlt-btn' onclick = 'deleteItem(event)'><i class="fa-solid fa-eraser"></i>erase</button><button class='dlt-btn' onclick = 'editItem(event)' ><i class="fa-solid fa-pen-to-square"></i>edit</button></li>`;
+    input.value = "";
+    lastMessage() 
+  }
+}
+
+
+// additem onclick function
+function addItem() {
+  list.innerHTML += `<li class = 'inputList' ><input onclick = 'delTag()' type="checkbox" id ="checkBoox"><input class = 'todo-inp' type="text" value='${input.value}' disabled ><button class='dlt-btn' onclick = 'deleteItem(event)' ><i class="fa-solid fa-eraser"></i></button><button class='dlt-btn' onclick = 'editItem(event)' ><i class="fa-solid fa-pen-to-square"></i></button></li>`;
+  input.value = "";
+  lastMessage() 
+}
+
+function delTag() {
+  let check = document.getElementById('checkBoox')
+if (check.checked) {
+    
+}
+
+}
+// deleteItem function
+function deleteItem(event) {
+  let listItem = event.target.parentElement;
+  console.log(listItem);
+  list.removeChild(listItem);
+}
+
+// Edit nd save function
+function editItem(event) {
+  let inputField = event.target.parentElement.childNodes[1]
+  console.log(inputField);
+  if (inputField.disabled) {
+    inputField.disabled = false;
+    button.innerHTML = iconEdit;
+    
+  } else {
+    inputField.disabled = true;
+    button.innerHTML = iconEdit;
+
+  }
+}
+
+// Delete all function
+function deleteAll() {
+  list.innerHTML = "";
+}
