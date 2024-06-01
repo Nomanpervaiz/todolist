fa30c2f70b7153d45fe9e1b29876d63e1a75fa40
Nomanpervaiz <nomanpervaiz035@gmail.com>
2024-06-01 10:48:18 +0500

    add

--- app.js
+++ app.js
@@ -11,26 +11,41 @@ function lastMessage() {
 }
 // add items on enter press function
 function addItemEnter(event) {
-  if (event.keyCode === 13) {
-    
-    list.innerHTML += `<li class = 'inputList' ><input onclick = 'delTag()'  type="checkbox" id ="checkBoox"><input class = 'todo-inp'  type="text" value='${input.value}' disabled><button class='dlt-btn' onclick = 'deleteItem(event)'><i class="fa-solid fa-eraser"></i>erase</button><button class='dlt-btn' onclick = 'editItem(event)' ><i class="fa-solid fa-pen-to-square"></i>edit</button></li>`;
-    input.value = "";
-    lastMessage() 
-  }
+
+    if (event.keyCode === 13) {
+      if (input.value !== '') {
+        
+        list.innerHTML += `<li class = 'inputList' ><input onclick = 'delTag(event)'  type="checkbox" id ="checkBoox"><input class = 'todo-inp'  type="text" value='${input.value}' disabled><button class='dlt-btn' onclick = 'deleteItem(event)'><i class="fa-solid fa-eraser"></i>erase</button><button class='dlt-btn' onclick = 'editItem(event)' ><i class="fa-solid fa-pen-to-square"></i>edit</button></li>`;
+        input.value = "";
+        lastMessage() 
+      }
+    }
+  
 }
 
 
 // additem onclick function
 function addItem() {
-  list.innerHTML += `<li class = 'inputList' ><input onclick = 'delTag()' type="checkbox" id ="checkBoox"><input class = 'todo-inp' type="text" value='${input.value}' disabled ><button class='dlt-btn' onclick = 'deleteItem(event)' ><i class="fa-solid fa-eraser"></i></button><button class='dlt-btn' onclick = 'editItem(event)' ><i class="fa-solid fa-pen-to-square"></i></button></li>`;
-  input.value = "";
-  lastMessage() 
+  if (input.value !== '') {
+    list.innerHTML += `<li class = 'inputList' ><input onclick = 'delTag(event)' type="checkbox" id ="checkBoox"><input class = 'todo-inp' type="text" value='${input.value}' disabled ><button class='dlt-btn' onclick = 'deleteItem(event)' ><i class="fa-solid fa-eraser"></i></button><button class='dlt-btn' onclick = 'editItem(event)' ><i class="fa-solid fa-pen-to-square"></i></button></li>`;
+    input.value = "";
+    lastMessage() 
+  }
 }
 
-function delTag() {
-  let check = document.getElementById('checkBoox')
+function delTag(event) {
+  let check = event.target
 if (check.checked) {
-    
+  let listItem = event.target.nextElementSibling  
+  listItem.style.textDecoration = 'line-through';
+  listItem.style.color = ' red';
+  
+  
+}else{
+  let listItem = event.target.nextElementSibling  
+  listItem.style.textDecoration = 'none';
+  listItem.style.color = 'grey';
+
 }
 
 }
@@ -44,14 +59,13 @@ function deleteItem(event) {
 // Edit nd save function
 function editItem(event) {
   let inputField = event.target.parentElement.childNodes[1]
-  console.log(inputField);
   if (inputField.disabled) {
     inputField.disabled = false;
-    button.innerHTML = iconEdit;
-    
+    event.target.innerText = 
+   'save'
   } else {
     inputField.disabled = true;
-    button.innerHTML = iconEdit;
+    
 
   }
 }
